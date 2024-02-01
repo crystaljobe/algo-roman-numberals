1054f9effe7b175feb5471d4681710d64c3a6f73
crystaljobe <crystaljobe29@gmail.com>
2024-02-01 07:57:26 -0700

    committing lazyRomanNumerals function

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,12 +1,6 @@
 /* Lazy Roman Numerals is where Roman Numerals are added together (9 is VIIII, 4 is IIII).
 Roman Numerals with Arabic Numeral counterpart:
-I -> 1
-V -> 5
-X -> 10
-L -> 50
-C -> 100
-D -> 500
-M -> 1000 */
+*/
 
 /* 1. Write a function toRomanLazy that takes in a single input, num(an arabic number).
 
@@ -26,8 +20,28 @@ M -> 1000 */
 
 9. return output */
 
+const romanNumerals = {
+  M: 1000,
+  D: 500,
+  C: 100,
+  L: 50,
+  X: 10,
+  V: 5,
+  I: 1,
+} 
+
 function toRomanLazy(num) {
-  return "";
+  let anser = "";
+  // iterate through values in obj and compare to num
+  for (rKey of Object.keys(romanNumerals)) {
+    const rValue = romanNumerals[rKey];
+    // if value is greateer than or equal 
+    if (num >= rValue) {
+      answer += rKey.repeat(Math.floor(num / rValue));
+      num = num % rValue;
+    }
+  }
+  return ""
 }
 
 function toRoman(num) {
